4952c725454c76631f3f003833ddbd96208eeb3e
KuznecovAndrey7777777 <107943133+KuznecovAndrey7777777@users.noreply.github.com>
2024-02-05 02:24:47 +0200

    save050220240224

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,72 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+import Notiflix from 'notiflix';
+
+const initialContacts = [
+    {
+        id: nanoid(),
+        name: 'Rosie Simpson',
+        number: '459-12-56',
+    },
+    {
+        id: nanoid(),
+        name: 'Hermione Kline',
+        number: '443-89-12',
+    },
+    {
+        id: nanoid(),
+        name: 'Eden Clements',
+        number: '645-17-79',
+    },
+    {
+        id: nanoid(),
+        name: 'Annie Copeland',
+        number: '227-91-26',
+    },
+];
+
+export const contactsSlice = createSlice({
+    name: 'contacts',
+    initialState: {
+        contacts: initialContacts,
+    },
+    reducers: {
+        addContact: {
+            reducer(state, action) {
+                if (
+                    state.contacts.find(
+                        existingContact => existingContact.name === action.payload.name
+                    )
+                ) {
+                    Notiflix.Notify.failure(
+                        `${action.payload.name} вже є у ваших контактах`
+                    );
+                } else {
+                    state.contacts.unshift(action.payload);
+
+                    Notiflix.Notify.success(
+                        `${action.payload.name} успішно додано до вашого телефонного букваря`
+                    );
+                }
+            },
+            prepare(name, number) {
+                return {
+                    payload: {
+                        name,
+                        number,
+                        id: nanoid(),
+                    },
+                };
+            },
+        },
+
+        deleteContact(state, action) {
+            state.contacts = state.contacts.filter(
+                contact => contact.id !== action.payload
+            );
+        },
+    },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export default contactsSlice.reducer;
